ce24c559f4876858ab103abe981e2cf99d805ae1
Merilee Wheelock <eelirem@gmail.com>
2017-05-24 08:33:26 -0400

    JS updates

--- js/scripts.js
+++ js/scripts.js
@@ -16,21 +16,23 @@ $(document).ready(function(){
 		// console.log(weatherUrl);
 		$.getJSON(weatherUrl, (weatherData)=>{
 			// console.log(weatherUrl);
-			var currTemp = weatherData.main.temp;
-			// var temps = {
-			// 	curr: weatherData.main.temp,
-			// 	max: weatherData.main.temp_max,
-			// 	min: weatherData.main.temp_min,
-			// }
+			var currTemp = Math.floor(weatherData.main.temp);
+			var maxTemp = Math.floor(weatherData.main.temp_max);
+			var minTemp = Math.floor(weatherData.main.temp_min);
+			
+
+			$(".currTempText").html('curr temp: '+currTemp+'&deg;F');
+			$(".maxTempText").html('max temp: '+maxTemp+'&deg;F');
+			$(".minTempText").html('min temp: '+minTemp+'&deg;F');
+			
+			// newHTML += '<div>The temp in ' + name + ' is currently ' + currTemp + '&deg;</div>';
 			var name = weatherData.name;
 			var icon = weatherData.weather[0].icon + '.png';
 			var desc = weatherData.weather[0].description;
-			var newHTML = '<img src="http://openweathermap.org/img/w/'+icon+'">' + desc;
-			newHTML += '<div>The temp in ' + name + ' is currently ' + currTemp + '&deg;</div>';
-			// console.log(newHTML);
-			$('#temp-info').html(newHTML);
+			var newHTML = '<div><img src="http://openweathermap.org/img/w/'+icon+'"></div>' + '<div><strong>'+desc+'</strong></div>';
+			$('.temp-info-icon').html(newHTML);
 			currentPercent = 0;
-			animateCircle(0,currTemp);
+			animateCircle(0,currTemp);	
 		});
 	});
 
@@ -40,6 +42,7 @@ $(document).ready(function(){
 	var currentPercent = 0;
 
 	function animateCircle(currentArc, currentTemp){
+		context.clearRect(0,0,500,500);
 		// console.log(currentArc);
 		// draw inner circle
 		context.fillStyle = "#ccc";
@@ -51,14 +54,32 @@ $(document).ready(function(){
 		// draw the outter line
 		// 5px wide line
 		context.lineWidth = 10;
-		context.strokeColor = "black";
+		// context.strokeStyle = "red";
 		context.beginPath();
 		context.arc(250,250,155,Math.PI*1.5,(Math.PI * 2 * currentArc) + Math.PI*1.5);
 		//Math.PI starts at 3 o'clock; 1.5*Math.PI starts at 12 o'clock
 		context.stroke();
 		// update the current percentage
 		currentPercent++;
-		if (currentPercent < assumedTemperature){
+		// console.log(currentArc);
+
+		if (currentTemp <= 32){
+			context.strokeStyle = "darkblue";
+		}else if (currentTemp < 80){
+			context.strokeStyle = "yellow";
+		}else if (currentTemp >= 80){
+			context.strokeStyle = "red";
+		}
+
+		if (currentTemp != undefined){
+			context.stroke();
+			context.font = "72px Khula";
+			context.fillStyle = "darkblue";
+			context.textBaseline = "top";
+			context.fillText(currentTemp+String.fromCharCode(176)+'F',190,210);
+		}
+
+		if (currentPercent < currentTemp){
 			requestAnimationFrame(function(){
 				animateCircle(currentPercent/100, currentTemp); // this will actually run the function. animateCircle without the () would run what the function returns
 			});
